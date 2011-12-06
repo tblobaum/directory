222faffc8f213c66d5f333163a1a401f2e48158f
Tom Blobaum <tblobaum@gmail.com>
2011-12-06 11:03:43 -0600

    prevent the module from caching

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ module.exports = function modulate(dirname, callback) {
   if (typeof dirname == 'function') {
     var dirname = module.parent.paths[0].split("node_modules")[0]
   }
+
   
   var paths = require('findit').sync(dirname)
   
   paths.forEach(function (path) {
-    if (!path.match(module.parent.id)) callback(require(path))
+    if (!path.match(module.parent.id)) {  
+      var filename = path.split(dirname)[1].split(".js")[0]
+      callback(require(path), filename)
+    }
   })
   
+  delete require.cache[__filename]
 }
 
